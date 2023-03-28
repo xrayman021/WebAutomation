3016f183cf15609234c9f2b49ccf61a4294cb23f
xrayman021 <82471086+xrayman021@users.noreply.github.com>
2023-03-28 14:28:56 -0500

    Got test site open and workinh

--- FindElementsByName.py
+++ FindElementsByName.py
@@ -1,1 +1,16 @@
 from selenium import webdriver
+import time
+
+firefox_driver_path = "/Users/boomb/Downloads/geckodriver-v0.32.2-win32"
+
+driver = webdriver.Firefox(firefox_driver_path)
+
+demo_website = "file://Users/boomb/Downloads/01 Source Files/01 Source Files/01 demo-website.html"
+
+driver.get(demo_website)
+
+driver.maximize_window()
+
+time.sleep(5)
+
+#driver.close()
